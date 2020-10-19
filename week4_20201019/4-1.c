edb037437009c1560b1bb60e26bef21b7e59ddaa
Zephyros <407405010@qq.com>
2020-10-19 08:54:21 +0800

    提交一次所有的修改

--- week4_20201019/4-1.c
+++ week4_20201019/4-1.c
@@ -6,10 +6,8 @@ int main(int argc, char **argv) {
 		printf("Usage: %s from-file to-file\n", argv[0]);
 		exit(1);
 	}
-	if ((from = open(argv[1], O_RDONLY)) < 0) err_exit(argv[1]);
-	if ((to = open(argv[2], O_WRONLY|O_CREAT|O_APPEND, 0644)) < 0) {
-		err_exit(argv[2]);
-	}
+	if ((from = open(argv[1], O_RDONLY)) < 0) exit(1);
+	if ((to = open(argv[2], O_WRONLY|O_CREAT|O_APPEND, 0644)) < 0) exit(1);
 	while (n = read(from, buf, sizeof(buf)) > 0) write(to, buf, n);
 	close(from);
 	close(to);
