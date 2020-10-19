f8d0951fe3cf7118e8c2c5e4c3d2243d669c8f6a
Zephyros <407405010@qq.com>
2020-10-19 08:46:27 +0800

    修改文件内容，week3加上了日期:

---
+++ week4_20201019/4-1.c
@@ -0,0 +1,18 @@
+#include "ch03.h"
+int main(int argc, char **argv) {
+	int n, from, to;
+	char buf[1024];
+	if (argc != 3) {
+		printf("Usage: %s from-file to-file\n", argv[0]);
+		exit(1);
+	}
+	if ((from = open(argv[1], O_RDONLY)) < 0) err_exit(argv[1]);
+	if ((to = open(argv[2], O_WRONLY|O_CREAT|O_APPEND, 0644)) < 0) {
+		err_exit(argv[2]);
+	}
+	while (n = read(from, buf, sizeof(buf)) > 0) write(to, buf, n);
+	close(from);
+	close(to);
+
+	return 0;
+}
