e04f263c5abdf747b8f8c74281d17e6135e3225e
Zephyros <407405010@qq.com>
2020-11-02 21:39:13 +0800

    20201102

--- homework_20201025/q3.c
+++ homework_20201025/q3.c
@@ -1,45 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
+#include <string.h>
+#include <math.h>
 
+void findljj(FILE *fp, int i) {
+    char str[60];
+    char buffer[20];
+    char buf[100];
+    int length[100];
+    int b = 0; //某数前面数据的长度
+    //读出数据的个数
+    memset(buf, 0, sizeof(buf));
+    memset(buffer, 0, sizeof(buffer));
+    memset(length, 0, sizeof(length));
+    fseek(fp, 0, SEEK_SET);
+    fread(buffer, 4, 1, fp);
 
+    int n; //数据个数
+    sscanf(buffer, "%d", &n);
 
-int read_word(FILE *fp) {
-    int n = 0;
-    int x1 = fgetc(fp);
-    int x2 = fgetc(fp);
-    int x3 = fgetc(fp);
-    int x4 = fgetc(fp);
-    n = (x1 << 24) + (x2 << 16) + (x3 << 8) + x4;
-    return n;
+    //读出数据的长度
+    fseek(fp, 4, SEEK_SET);
+    fread(buf, 4 * n, 1, fp);
+    int j, w = 0;
+    for (j = 0; j < 4 * n; j += 4) {
+        if (buf[j] != ' ' && buf[j + 1] == ' ') {
+            length[w++] = (int)(buf[j] - '0');
+        }
+        if (buf[j] != ' ' && buf[j + 1] != ' ' && buf[j + 2] == ' ') {
+            length[w++] = (int)((buf[j] - '0') * 10 + buf[j + 1] - '0');
+        }
+        if (buf[j] != ' ' && buf[j + 1] != ' ' && buf[j + 2] != ' ' && buf[j + 3] == ' ') {
+            length[w++] = (int)((buf[j] - '0') * 100 + (buf[j + 1] - '0') * 10 + (buf[j + 2] - '0'));
+        }
+        if (buf[j] != ' ' && buf[j + 1] != ' ' && buf[j + 2] != ' ' && buf[j + 3] != ' ') {
+            length[w++] = (int)((buf[j] - '0') * 1000 + (buf[j + 1] - '0') * 100 + (buf[j + 2] - '0') * 10 + (buf[j + 3] - '0'));
+        }
+    }
+    int l;
+    for (l = 0; l < i - 1; l++) {
+        b = length[l] + b;
+    }
+    //读出第i个数据的内容
+
+    fseek(fp, (n + 1) * 4 + b, SEEK_SET);
+    if (fgets(str, length[i - 1] + 1, fp) != NULL) {
+        puts(str);
+    }
+    else {
+        printf("error");
+    }
 }
-
 int main() {
-    char* filename = "input1.txt";
-    int k = 0;   
-    int m = 0;
-    int i,j;
-    FILE *fp = NULL;
-    fp = fopen(filename, "r");
+    FILE *fp;
+    fp = fopen("file.txt", "w+");
+    int i; //第几个数据
+    scanf("%d", &i);
+    //写入数据
+    fputs("10  ", fp);
 
-    int n = read_word(fp);
-    int *array;
-    array = (int*)malloc(n * sizeof(int));
+    fputs("10  ", fp);
+    fputs("2   ", fp);
+    fputs("2   ", fp);
+    fputs("5   ", fp);
+    fputs("1   ", fp);
+    fputs("1   ", fp);
+    fputs("1   ", fp);
+    fputs("1   ", fp);
+    fputs("1   ", fp);
+    fputs("1   ", fp);
+    fputs("C Programming Langauge abcdefghijk", fp);
 
-    for(i = 0; i < n; i++){
-        array[i] = read_word(fp);
-    }
-    printf("在%d个记录中,想输出第几个数据：",n);
-    scanf("%d", &k);
-    for(int i = 0; i < k - 1; i++){
-        fseek(fp, 4 * array[i], SEEK_CUR);
-    }
-
-    for(j = 0; j < array[k - 1]; j++){
-        printf("%d", read_word(fp));
-    }
-    printf("\n");
-
+    findljj(fp, i);
     fclose(fp);
+
     return 0;
 }
